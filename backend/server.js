4ff6eb9d0642b0b9a45f28432fcce2f2805a1296
Supremo1717 <ralphmatthewmanabat@gmail.com>
2024-07-27 09:34:44 +0800

    Inserting data to server using axios

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000 || process.env.port
 app.use(express.json())
+const db = require('./connection')
+app.use(cors())
+app.post('/insertData',async (req, res)=>{
+    try {
+        const {lat, lng} = req.body
+        const query = await db(`Insert into coords_data ( lat, lng) values('${lat}', '${lng}')`)
+        res.json({message:"Success!"}).status(200)
+        console.log(lng)
+    } catch (error) {
+        console.log(error)
+        res.json({message:"Error!"})
+    }
 
-
-app.get('/', (req, res)=>{
-    res.send('hello')
 })
 app.listen(port, ()=>{
     console.log(`Server started at port ${port}`)
